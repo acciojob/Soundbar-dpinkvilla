96506d3e0cb2b44225f2991531f4f817acfe45b2
dpinkvilla <117904994+dpinkvilla@users.noreply.github.com>
2024-03-23 12:18:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let applause=document.getElementById("id-1").addEventListener("click", function() {
+	let audio1=document.getElementById("sound-1"); 
+	audio1.play();
+    
+});
+
+let stop1=document.querySelector(".stop").addEventListener("click",function() {
+	let stopaudio=document.querySelectorAll("#sounds audio");
+	stopaudio.forEach((Element) =>Element.pause()); 
+    
+});
+
